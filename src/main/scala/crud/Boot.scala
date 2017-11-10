332e26bc60e11f3282d399808a424cda790382dc
Zulek <zulek24@gmail.com>
2017-11-10 18:31:41 +0800

    Update Boot.scala

--- src/main/scala/crud/Boot.scala
+++ src/main/scala/crud/Boot.scala
@@ -12,14 +12,11 @@ import scala.concurrent.duration._
 
 object Boot extends App with DatabaseConfig {
 
-
-  // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("on-spray-can")
   implicit val executionContext = system.dispatcher
 
   val service = system.actorOf(Props[RestInterface])
 
   implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
   IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
 }
